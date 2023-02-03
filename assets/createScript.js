d79d77697e44365b8e7dd10f6ee1436d004a9280
victorgarcia00 <victor.garcia@juanxxiii.net>
2023-02-03 17:29:29 +0100

    otra subida de archivos

--- assets/createScript.js
+++ assets/createScript.js
@@ -2,19 +2,19 @@
 
 // Declaracion de variables
 var body = document.getElementsByTagName('body')[0];
-var numNavItem=4;
-var arrDatosNav = ['Inicio','Reservas','Reseñas','Categorias','Recordatorios'];
+var numNavItem = 4;
+var arrDatosNav = ['Inicio', 'Reservas', 'Reseñas', 'Categorias', 'Recordatorios'];
 
 //Declaracion de funciones
 function createHeader(arrDatosNav) {
     let header = document.createElement('header');
-    let nav =document.createElement('nav');
+    let nav = document.createElement('nav');
     let ul = document.createElement('ul');
     //LOGO DE LA COMPAÑIA
     let logo = document.createElement('div');
     logo.classList.add('logo');
     let imgLogo = document.createElement('img');
-    imgLogo.src = '#';
+    imgLogo.src = '../images/logo.jpg';
     logo.appendChild(imgLogo);
     //PERFIL DEL USUARIO
     let profile = document.createElement('div');
@@ -29,26 +29,34 @@ function createHeader(arrDatosNav) {
     let username = document.createElement('p');
     let usernameText = document.createTextNode('Nombre usuario');
     let closeSessions = document.createElement('a');
-    closeSessions.textContent='Cerrar Sesion';
-    closeSessions.href='#';
+    closeSessions.textContent = 'Cerrar Sesion';
+    closeSessions.href = '#';
     username.append(usernameText);
     userInfo.appendChild(username);
     userInfo.appendChild(closeSessions);
     profile.appendChild(divImgProfile);
     profile.appendChild(userInfo);
     for (let index = 0; index < numNavItem; index++) {
-        let navItem = document.createElement('li'); 
+        let navItem = document.createElement('li');
         let navItemLink = document.createElement('a');
-        navItemLink.setAttribute('href','#');
+        navItemLink.setAttribute('href', '#');
         let navItemLinkText = document.createTextNode(arrDatosNav[index]);
         navItemLink.append(navItemLinkText);
         navItem.appendChild(navItemLink);
-        ul.appendChild(navItem);    
+        ul.appendChild(navItem);
     }
+    let searchBar = document.createElement('input');
+    searchBar.setAttribute('type', 'search');
+    searchBar.setAttribute('placeholder', 'Search')
     nav.appendChild(ul);
     header.appendChild(logo);
     header.appendChild(nav);
     header.appendChild(profile);
+    header.appendChild(searchBar);
     body.appendChild(header);
+}
+
+function createMain() {
+    let main = document.createElement('main');
 
 }
